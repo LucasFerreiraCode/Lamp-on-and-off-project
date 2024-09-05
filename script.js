fcc3e0e206b9243885ebaf77e2b019c9464681e8
LucasFerreira92 <lucasferreirasurf13@gmail.com>
2024-09-05 15:01:17 -0300

    ajuste-modo-dar-and-light

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-function myfunon(){
-    document.getElementById("bulb").src="https://i.postimg.cc/h4pB57c8/Lampada-acesa.webp";
+function myfunon() {
+    document.getElementById("bulb").src = "https://i.postimg.cc/h4pB57c8/Lampada-acesa.webp";
 }
 
-function myfunonoff(){
-    document.getElementById("bulb").src="https://i.postimg.cc/mkFyCHwx/Lampada-apagada.webp";
+function myfunonoff() {
+    document.getElementById("bulb").src = "https://i.postimg.cc/mkFyCHwx/Lampada-apagada.webp";
 }
+
+ const botao = document.getElementById('botao')
+
+ botao.addEventListener('change', ()=>{
+    document.body.classList.toggle('dark')
+ })
